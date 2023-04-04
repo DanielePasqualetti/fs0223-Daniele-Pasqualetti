b30460e64aa97563ea4869c3d55b4647e9f508f8
DanielePasqualetti <daniele.pasqualetti94@gmail.com>
2023-04-04 18:07:40 +0200

    display

---
+++ m1/s2/g2/pomeriggio/script.js
@@ -0,0 +1,17 @@
+
+function addNumber(n){
+
+    let display = document.getElementById("display");
+
+    display.value += n.innerHTML;
+    
+}
+
+/*function addNumber(n){
+
+    let display = document.getElementById("display");
+
+    display.value += n;
+    
+}
+*/
