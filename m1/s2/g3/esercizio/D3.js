d1921c4579e06a26378ebb18ddcf0b6cfacd8bd5
DanielePasqualetti <daniele.pasqualetti94@gmail.com>
2023-04-05 22:00:34 +0200

    esercizio finito

--- m1/s2/g3/esercizio/D3.js
+++ m1/s2/g3/esercizio/D3.js
@@ -96,86 +96,22 @@
   */
   
   /* SCRIVI QUI LA TUA RISPOSTA */
-  // {
-  // let num1 = 66;
-  // let num2 = 48;
-  // let num3 = 14;
-  // let arr = [];
-  
-  // if (arr == []){
-  //   arr.push(num1, num2, num3)
-  //   console.log(arr)
-  // }
-  
-  // arr.sort(function(a,b){
-  //   return a - b
-  // });
-  
-  // console.log(arr)
-  // }
-  
-  /*
-  
-  let num = [1,100,20,22,101];
-  
-          num.sort(function(a,b){
-              return a - b
-          });
-  
-          console.log(num);
-  
-  
-  
-  
-  if(num1 > num2 && num1 > num3 && num2 > num3){
-    arr.unshift(num1)
-    console.log(arr)
-  }else if(num2 > nume3){
-    arr.push(num2)
-    console.log(arr)
-  }
-  
-  
+  {
+    let num1 = 66;
+    let num2 = 48;
+    let num3 = 14;
+    let arr = [];
     
-  }
-  
-  let a = 2 
-  let b = 4
-  let c = 8
-  
-  if (a > b && a > c)
-  {
-          if (b>c)
-          {
-              console.log(La sequenza sarà ${a} ${b} ${c});
-          }
-          else
-          {
-              console.log(La sequenza sarà ${a} ${c} ${b});
-          }
-  }
-  else if (b > a && b > c)
-  {
-          if (a>c)
-          {
-               console.log(la sequenza sarà ${b} ${a} ${c});
-          }
-          else
-          {
-               console.log(la sequenza sarà ${b} ${c} ${a});
-          }
-  }
-  else if (c > a && c > b)
-  {
-          if (a>b)
-          {
-              console.log(la sequenza sarà ${c} ${a} ${b});
-          }
-          else
-          {
-              console.log(la sequenza sarà ${c} ${b} ${a});
-          }
-  }*/
+    if (arr.length == 0){
+      arr.push(num1, num2, num3)
+    }
+    
+    arr.sort(function(a,b){
+      return b - a
+    });
+    
+    console.log(arr)
+    }
   
   /* ESERCIZIO 8
     Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
@@ -284,16 +220,16 @@
   /* SCRIVI QUI LA TUA RISPOSTA */
   {
     let arr = [];
-    arr.push(1,2,3,4,5,6,7,8,9,10)
+    arr.push(1,2,3,4,5,6,7,8,9,10);
     console.log(arr);
   }
   /* ESERCIZIO 15
     Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
   */
-    {
-      let arr = [];
-      arr.push(1,2,3,4,5,6,7,8,9,10)
-      arr.pop()
-      arr.push(100)
-      console.log(arr);
-    }
+  {
+    let arr = [];
+    arr.push(1,2,3,4,5,6,7,8,9,10)
+    arr.pop()
+    arr.push(100)
+    console.log(arr);
+  }
