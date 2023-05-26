bbf175d1b385cd5be40a6c9a97fc03a4a7617893
DanielePasqualetti <129083893+DanielePasqualetti@users.noreply.github.com>
2023-05-26 15:25:32 +0200

    fix

--- m3/s1/g2/lezione/OOP/extends.ts
+++ m3/s1/g2/lezione/OOP/extends.ts
@@ -1,51 +1,45 @@
-class Pizza{
+class Pizza {
+ protected gusto: string;
+ protected prezzo: number;
+ private formati: string[] = ["Normale", "Mini", "Mezzo metro"];
 
-    protected gusto:string;
-    protected prezzo:number;
-    private formati:string[] = ['Normale', 'Mini', 'Mezzo metro'];
+ constructor(_gusto: string, _prezzo: number) {
+  this.gusto = _gusto;
+  this.prezzo = _prezzo;
+ }
 
-    constructor(_gusto:string, _prezzo:number){
-        this.gusto = _gusto;
-        this.prezzo = _prezzo;        
-    }
-
-    public informazioni():string{
-        return `Gusto:${this.gusto}, Prezzo: ${this.prezzo} €`;
-    }
-
+ public informazioni(): string {
+  return `Gusto:${this.gusto}, Prezzo: ${this.prezzo} €`;
+ }
 }
 
-let margherita:Pizza = new Pizza('Margherita',5);
+let margherita: Pizza = new Pizza("Margherita", 5);
 
 //console.log(margherita.prezzo); //errore
 
-
 console.log(margherita.informazioni());
 
+class PizzaSpeciale extends Pizza {
+ protected altezzaCornicione: number;
 
-class PizzaSpeciale extends Pizza{
+ constructor(_gusto: string, _prezzo: number, _altezzaCornicione: number) {
+  super(_gusto, _prezzo);
+  this.altezzaCornicione = _altezzaCornicione;
+ }
 
-    protected altezzaCornicione:number;
+ //  mostraFormati() {
+ //   console.log(this.formati); //errore poiché la prop formati è private ed appartiene alla superclasse
+ //  }
 
-    constructor(_gusto:string, _prezzo:number, _altezzaCornicione:number){
-        super(_gusto, _prezzo);
-        this.altezzaCornicione = _altezzaCornicione;
-    }
-
-    mostraFormati(){
-        console.log(this.formati)//errore poiché la prop formati è private ed appartiene alla superclasse
-    }
-
-    public informazioni(): string {
-        let datoSuperClasse = super.informazioni();
-        return `${datoSuperClasse}, Altezza Cornicione: ${this.altezzaCornicione} `;
-    }
-
+ public informazioni(): string {
+  let datoSuperClasse = super.informazioni();
+  return `${datoSuperClasse}, Altezza Cornicione: ${this.altezzaCornicione} `;
+ }
 }
 
-let diavola = new PizzaSpeciale('Diavola',1,5);
+let diavola = new PizzaSpeciale("Diavola", 1, 5);
 
 console.log(diavola);
 console.log(diavola.informazioni());
 
-diavola.mostraFormati();
+// diavola.mostraFormati();
